a53e14b89f0df98a82afd8b43abcb4a8edf9e4da
Richard Marbach <rmarbach@gmail.com>
2022-11-17 07:37:18 +0100

    Fix method call

--- lua/extract-ruby-constant/init.lua
+++ lua/extract-ruby-constant/init.lua
@@ -1,60 +1,60 @@
-local ts = vim.treesitter.query
+local ts = vim.treesitter
 
 local M = {}
 
 local function find_next_module_constant()
-	local node = ts.get_node_at_cursor()
+  local node = ts.get_node_at_cursor()
 
-	if node:type() == "constant" and node:parent() and node:parent():type() == "assignment" then
-		return node
-	end
+  if node:type() == "constant" and node:parent() and node:parent():type() == "assignment" then
+    return node
+  end
 
-	while node ~= nil and not vim.tbl_contains({ "class", "module" }, node:type()) do
-		node = node:parent()
-	end
+  while node ~= nil and not vim.tbl_contains({ "class", "module" }, node:type()) do
+    node = node:parent()
+  end
 
-	return node
+  return node
 end
 
 local function get_parent_class_or_module(node)
-	node = node:parent()
-	while node and not vim.tbl_contains({ "class", "module" }, node:type()) do
-		node = node:parent()
-	end
-	return node
+  node = node:parent()
+  while node and not vim.tbl_contains({ "class", "module" }, node:type()) do
+    node = node:parent()
+  end
+  return node
 end
 
 local function get_constant_name(node)
-	if node:type() == "constant" then
-		return ts.get_node_text(node)[1]
-	end
-	return ts.get_node_text(node:child(1))[1]
+  if node:type() == "constant" then
+    return ts.query.get_node_text(node)[1]
+  end
+  return ts.query.get_node_text(node:child(1))[1]
 end
 
 local function get_full_constant_name()
-	local node = find_next_module_constant()
-	if not node then
-		return
-	end
+  local node = find_next_module_constant()
+  if not node then
+    return
+  end
 
-	local names = {}
-	while node do
-		table.insert(names, get_constant_name(node))
-		node = get_parent_class_or_module(node)
-	end
+  local names = {}
+  while node do
+    table.insert(names, get_constant_name(node))
+    node = get_parent_class_or_module(node)
+  end
 
-	names = vim.fn.reverse(names)
-	return table.concat(names, "::")
+  names = vim.fn.reverse(names)
+  return table.concat(names, "::")
 end
 
 function M.extract()
-	return get_full_constant_name()
+  return get_full_constant_name()
 end
 
 function M.yank()
-	local name = M.extract()
-	vim.fn.setreg(vim.v.register, name)
-	vim.fn.setreg("0", name)
+  local name = M.extract()
+  vim.fn.setreg(vim.v.register, name)
+  vim.fn.setreg("0", name)
 end
 
 return M
