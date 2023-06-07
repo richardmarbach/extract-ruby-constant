8ca2c2c9c7a2f20bba406067232a94b9c26234e9
Richard Marbach <rmarbach@gmail.com>
2023-06-07 07:24:23 +0200

    Remove deprecated call

--- lua/extract-ruby-constant/init.lua
+++ lua/extract-ruby-constant/init.lua
@@ -1,61 +1,60 @@
 local ts = require("nvim-treesitter.ts_utils")
-local query = vim.treesitter.query
 
 local M = {}
 
 local function find_next_module_constant()
-  local node = ts.get_node_at_cursor()
+	local node = ts.get_node_at_cursor()
 
-  if node:type() == "constant" and node:parent() and node:parent():type() == "assignment" then
-    return node
-  end
+	if node:type() == "constant" and node:parent() and node:parent():type() == "assignment" then
+		return node
+	end
 
-  while node ~= nil and not vim.tbl_contains({ "class", "module" }, node:type()) do
-    node = node:parent()
-  end
+	while node ~= nil and not vim.tbl_contains({ "class", "module" }, node:type()) do
+		node = node:parent()
+	end
 
-  return node
+	return node
 end
 
 local function get_parent_class_or_module(node)
-  node = node:parent()
-  while node and not vim.tbl_contains({ "class", "module" }, node:type()) do
-    node = node:parent()
-  end
-  return node
+	node = node:parent()
+	while node and not vim.tbl_contains({ "class", "module" }, node:type()) do
+		node = node:parent()
+	end
+	return node
 end
 
 local function get_constant_name(node)
-  if node:type() == "constant" then
-    return query.get_node_text(node, 0)
-  end
-  return query.get_node_text(node:child(1), 0)
+	if node:type() == "constant" then
+		return vim.treesitter.get_node_text(node, 0)
+	end
+	return vim.treesitter.get_node_text(node:child(1), 0)
 end
 
 local function get_full_constant_name()
-  local node = find_next_module_constant()
-  if not node then
-    return
-  end
+	local node = find_next_module_constant()
+	if not node then
+		return
+	end
 
-  local names = {}
-  while node do
-    table.insert(names, get_constant_name(node))
-    node = get_parent_class_or_module(node)
-  end
+	local names = {}
+	while node do
+		table.insert(names, get_constant_name(node))
+		node = get_parent_class_or_module(node)
+	end
 
-  names = vim.fn.reverse(names)
-  return table.concat(names, "::")
+	names = vim.fn.reverse(names)
+	return table.concat(names, "::")
 end
 
 function M.extract()
-  return get_full_constant_name()
+	return get_full_constant_name()
 end
 
 function M.yank()
-  local name = M.extract()
-  vim.fn.setreg(vim.v.register, name)
-  vim.fn.setreg("0", name)
+	local name = M.extract()
+	vim.fn.setreg(vim.v.register, name)
+	vim.fn.setreg("0", name)
 end
 
 return M
